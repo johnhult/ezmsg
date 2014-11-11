f56c0dc5a739d7f24c0558426f5cf7676a980d8a
John <johnhult89@gmail.com>
2014-11-11 20:15:28 +0100

    data for profiles added

---
+++ resources/SQL/person.sql
@@ -0,0 +1,69 @@
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('a450278e-241e-4206-b3cf-ace05911a7da', 'An-Cii', 'Hult', 1, 'ancii@forumskill.com', 'pASS123', '3f6fb455-27a3-4edb-8d25-5009050d4467.jpg', 'null')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('30212293-442e-46e1-ad41-f341d7d0a809', 'Elisabeth', 'Silverplats', 0, 'elisabeth@mamasretro.com', 'pASS123', '0d50f162-9da5-46fc-a6aa-7e7b13196b47.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('162fbea2-a0bf-4744-9716-2f265eddbeab', 'Linnea', 'Thoor', 1, 'linnea@forumskill.com', 'pASS123', '1e95aaea-f5b2-44bc-a2bd-294d4be11f54.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('9765862e-268a-4465-a238-3c7b7373d438', 'Catrin', 'Schultz', 1, 'catrin@mamasretro.com', 'pASS123', '00b0ac4c-19df-48fb-bcdb-37871c68152a.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('64ce7298-5330-497b-80c8-24613d970517', 'Ann-Louise', 'Slotthag', 1, 'ann-louise@mamasretro.com', 'pASS123', '1a76a869-2bea-4d3a-8cc1-167838b16751.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('74e7da80-51df-4aca-a1c5-ffefaa8a5347', 'Johanna', 'Friberg', 1, 'johanna@mamasretro.com', 'pASS123', 'ceddb0f6-cd6a-4320-a087-c70ba1a3664f.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('282d21c6-696d-45fa-a8ab-f1abcf8615e1', 'Erika', 'Gustafsson', 1, 'erika@forumskill.com', 'pASS123', '83fd059e-aeef-42b8-b593-168ff72bd30e.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('c0911e78-be7a-478b-8aae-c7994d71aa14', 'Jos', 'Rossling', 1, 'jos@forumskill.com', 'pASS123', 'a0f041e1-faba-4d04-bf31-486ee1691caa.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('49c66b88-4bc2-411b-a34a-58adff48ed2c', 'Mia', 'Bobzow', 1, 'mia@forumskill.com', 'pASS123', 'b5270214-4cdc-45c4-994f-403256ea0183.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('d94d9bf3-b082-417f-949a-3283c025f64d', 'Paulina', 'Hammarstrand', 1, 'paulina@forumskill.com', 'pASS123', 'b46dfd2d-ecd3-4445-81cd-7bed8e6612ea.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('0d2e1b1e-2298-4d6d-810c-70ab83fbd647', 'Ulrica', 'Wänman', 1, 'ulrica@forumskill.com', 'pASS123', '7e90574c-b583-44b6-8b3e-fcb047b54440.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('7413379e-66e2-4748-a0a5-8432d54ec380', 'Bea', 'Alger', 1, 'bea@forumskill.com', 'pASS123', 'f3bc271f-5248-43d7-bcfb-cc2fadc77442.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('a096b041-96ac-49c9-a6db-ad28f499eb91', 'Sheila', 'Silvestre', 1, 'sheila@mamasretro.com', 'pASS123', '969e3102-d695-47c1-a59f-fcac5d57c871.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
+
+INSERT into person
+(id, first_name, last_name, admin, email, password, picture, creator_id)
+VALUES
+('5b1504f6-5dc0-4539-9881-d850f4d59ae5', 'Sabina', 'Tanbäck', 1, 'sabina@mamasretro.com', 'pASS123', 'cd7dcba3-af2c-4461-bf5b-9e00ea324081.jpg', 'a450278e-241e-4206-b3cf-ace05911a7da')
