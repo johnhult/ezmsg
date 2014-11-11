2fff4597f643b68d30c1d11a8db92d500cba7d54
Anders Levin <anders@tyrant.se>
2014-11-11 19:02:25 +0100

    Layout update

--- WebContent/js/ezmsg.js
+++ WebContent/js/ezmsg.js
@@ -1,3 +1,33 @@
+var zIndex = 1000;
+var profileExpandsOverRows = 4;
+var profileExpandsOverCols = 2;
+var changing = false;
+
+var messageArea = '';
+
+
+$('.profile img').click(function() {
+	if (!changing) {
+		changing = true;
+		var $t = $(this).parent();
+		if (!$t.hasClass('maximized')) {
+			$t.attr('data-orgwidth', $t.outerWidth());
+			$t.attr('data-orgheight', $t.outerHeight());
+			var width = ($t.outerWidth() * profileExpandsOverCols) + ((profileExpandsOverCols - 1) *10);
+			var height = ($t.outerHeight() * profileExpandsOverRows) + ((profileExpandsOverRows - 1) * 10);
+			$t.addClass('maximized').css('z-index', zIndex++).animate({'width': width + 'px', 'height' : height + 'px'}, 200, function() {
+				$t.closest('.js-masonry').masonry();
+				changing = false;
+			});
+			$t.append(messageArea).find('.message-area,.message-send-area').show();
+		} else {
+			$t.removeClass('maximized').animate({'width': $t.attr('data-orgwidth') + 'px', 'height' : $t.attr('data-orgheight') + 'px'}, 200, function() {
+				$t.closest('.js-masonry').masonry();
+				changing = false;
+			});
+		}
+	}
+});
 $('.action-send').click(function() {
 	try {
 		var $t = $(this);
@@ -18,6 +48,6 @@ $('.action-slide').click(function() {
 	var $t = $(this);
 	var $content = $('.' + $t.attr('data-slide-id'));
 	if ($content.length > 0) {
-		$content.parent().stop().animate({'left': -$content.position().left + "px"});
+		$content.parent().stop().animate({'left': -$content.position().left + 'px'});
 	}
 });
